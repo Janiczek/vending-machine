942f2af75b66f9a33e44b4d90bc35c23d85e2675
Martin Janiczek <martin@janiczek.cz>
2017-07-03 17:34:38 +0200

    initial

---
+++ VendingMachine.elm
@@ -0,0 +1,45 @@
+module VendingMachine exposing (Model, Msg(..), init, update)
+
+
+type Msg
+    = AddCoins Int
+    | Cancel
+    | Buy
+    | TakeProduct
+
+
+type alias Model =
+    { currentCoins : Int
+    , productPrice : Int
+    , isProductVended : Bool
+    }
+
+
+init : Model
+init =
+    { currentCoins = 0
+    , productPrice = 25
+    , isProductVended = False
+    }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCoins amount ->
+            { model | currentCoins = model.currentCoins + amount }
+
+        Cancel ->
+            { model | currentCoins = 0 }
+
+        Buy ->
+            if model.currentCoins >= model.productPrice then
+                { model
+                    | currentCoins = 0
+                    , isProductVended = True
+                }
+            else
+                model
+
+        TakeProduct ->
+            { model | isProductVended = False }
